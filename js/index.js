72ed7cf5aa7196b96543182d835bd7d31abafde2
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-23 13:00:28 -0800

    aw

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
+const RickAndMorty = ["https://embed.wave.video/U0ADydR2Xlsa4M8Z","https://embed.wave.video/szETBpxLiCE1cJCm"];
+
 function index() {
-    window.location.assign("Empirez.online");
+    window.location.assign("index.html");
 }
 
 function home() {
-    window.location.assign("index.html");
+    window.location.assign("VideoPlayer.html");
 }
 
 if (window.localStorage.getItem("CurrentEpisode")) {
@@ -13,6 +15,14 @@ if (window.localStorage.getItem("CurrentEpisode")) {
 }
 
 function Page_Load() {
+    for (var i = 0; i < RickAndMorty.length; i++) {
+        var x = document.createElement("option");
+        var v = document.getElementsByClassName("Episodes").item(0).length + 1;
+        x.text = "Episode " + v;
+        x.value = RickAndMorty[v-1]
+        document.getElementsByClassName("Episodes").item(0).appendChild(x);
+    }
+
     document.getElementsByClassName("VideoPlayer").item(0).src = Episode;
     document.getElementsByClassName("Episodes").item(0).value = Episode;
 }
@@ -24,4 +34,14 @@ function Page_Unload() {
 function EpisodeSelect() {
     Episode = document.getElementsByClassName("Episodes").item(0).value;
     document.getElementsByClassName("VideoPlayer").item(0).src = Episode;
+}
+
+function ShowSelect() {
+    if (document.getElementsByClassName("Shows").item(0)) {
+        var x = document.createElement("option");
+        var v = document.getElementsByClassName("Episodes").item(0).length + 1;
+        x.text = "Episode " + v;
+        x.value = RickAndMorty[v-1]
+        document.getElementsByClassName("Episodes").item(0).appendChild(x);
+    }
 }
