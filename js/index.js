6100dd43ad265e4df6431bcb8b3c0a14d7d0c327
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-23 22:12:55 -0800

    abc

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-const RickAndMorty = ["https://embed.wave.video/U0ADydR2Xlsa4M8Z","https://embed.wave.video/szETBpxLiCE1cJCm"];
+const ShowList = {
+    "RickAndMorty": ["https://embed.wave.video/U0ADydR2Xlsa4M8Z","https://embed.wave.video/szETBpxLiCE1cJCm"],
+    "FamilyGuy": ["https://embed.wave.video/1v3TRgupkK55szpC"]
+}
 
 function index() {
     window.location.assign("index.html");
@@ -14,12 +17,20 @@ if (window.localStorage.getItem("CurrentEpisode")) {
     Episode = "https://embed.wave.video/U0ADydR2Xlsa4M8Z";
 }
 
+if (window.localStorage.getItem("CurrentShow")) {
+    Show = window.localStorage.getItem("CurrentShow");
+}else {
+    Show = "RickAndMorty";
+}
+
 function Page_Load() {
-    for (var i = 0; i < RickAndMorty.length; i++) {
+    document.getElementsByClassName("Shows").item(0).value = Show;
+
+    for (var i = 0; i < ShowList[Show].length; i++) {
         var x = document.createElement("option");
         var v = document.getElementsByClassName("Episodes").item(0).length + 1;
         x.text = "Episode " + v;
-        x.value = RickAndMorty[v-1]
+        x.value = ShowList[Show][v-1]
         document.getElementsByClassName("Episodes").item(0).appendChild(x);
     }
 
@@ -29,6 +40,7 @@ function Page_Load() {
 
 function Page_Unload() {
     window.localStorage.setItem("CurrentEpisode", Episode)
+    window.localStorage.setItem("CurrentShow", Show)
 }
 
 function EpisodeSelect() {
@@ -37,11 +49,7 @@ function EpisodeSelect() {
 }
 
 function ShowSelect() {
-    if (document.getElementsByClassName("Shows").item(0)) {
-        var x = document.createElement("option");
-        var v = document.getElementsByClassName("Episodes").item(0).length + 1;
-        x.text = "Episode " + v;
-        x.value = RickAndMorty[v-1]
-        document.getElementsByClassName("Episodes").item(0).appendChild(x);
-    }
+    Show = document.getElementsByClassName("Shows").item(0).value;
+    Episode = ShowList[Show][0];
+    document.location.reload(true);
 }
