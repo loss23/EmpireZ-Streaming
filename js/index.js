ac54fe5f1c1a3208da3b4decad45423de56de072
loss23 <61843498+loss23@users.noreply.github.com>
2022-12-25 20:14:55 -0800

    SEXY SPONGEMAN SEX BOB

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
-const wave_ShowList = {
-    "RickAndMorty": ["https://embed.wave.video/U0ADydR2Xlsa4M8Z","https://embed.wave.video/szETBpxLiCE1cJCm",],
-    "FamilyGuy": ["https://embed.wave.video/1v3TRgupkK55szpC", "https://embed.wave.video/1f6rtAHOBbGs3BG7"],
+const DataBase = {
+    "Shows": {
+        "RickAndMorty": {
+            "1": ["https://embed.wave.video/U0ADydR2Xlsa4M8Z", "https://embed.wave.video/szETBpxLiCE1cJCm"]
+        },
+        "FamilyGuy": {
+            "1" : ["https://embed.wave.video/1v3TRgupkK55szpC", "https://embed.wave.video/1f6rtAHOBbGs3BG7"]
+        },
+        "Spongebob": {
+            "1" : ["https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep1.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep2.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep3.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep4.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep5.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep6.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep7.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep8.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep9.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep10.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep11.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep12.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep13.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep14.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep15.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep16.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep17.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep18.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep19.mp4", "https://filebin.net/xrd6axgrzvozhila/Spongebob_Ep20.mp4"]
+        }
+    },
+    "Movies": {
+        "TEST": ""
+    }
 }
 
-const MovieList = {
-    
-}
 
 function index() {
     window.location.assign("index.html");
@@ -21,10 +30,8 @@ function info() {
 
 if (window.localStorage.getItem("CurrentEpisode")) {
     Episode = window.localStorage.getItem("CurrentEpisode");
-    Movie = window.localStorage.getItem("CurrentMovie");
 }else {
-    Episode = "https://embed.wave.video/U0ADydR2Xlsa4M8Z";
-    Movie = "https://embed.wave.video/szETBpxLiCE1c";
+    Episode = DataBase["Shows"]["RickAndMorty"]["1"][1]
 }
 
 if (window.localStorage.getItem("CurrentShow")) {
@@ -33,24 +40,41 @@ if (window.localStorage.getItem("CurrentShow")) {
     Show = "RickAndMorty";
 }
 
+if (window.localStorage.getItem("CurrentSeason")) {
+    Season = window.localStorage.getItem("CurrentSeason");
+}else {
+    Season = "1";
+}
+
+
 function Page_Load() {
     document.getElementsByClassName("Shows").item(0).value = Show;
 
-    for (var i = 0; i < wave_ShowList[Show].length; i++) {
+    for (var i = 0; i < Object.keys(DataBase["Shows"][Show]).length; i++) {
+        var x = document.createElement("option");
+        var v = document.getElementsByClassName("Seasons").item(0).length + 1;
+        x.text = "Season " + v;
+        x.value = v;
+        document.getElementsByClassName("Seasons").item(0).appendChild(x);
+    }
+
+    for (var i = 0; i < DataBase["Shows"][Show][Season].length; i++) {
         var x = document.createElement("option");
         var v = document.getElementsByClassName("Episodes").item(0).length + 1;
         x.text = "Episode " + v;
-        x.value = wave_ShowList[Show][v-1]
+        x.value = DataBase["Shows"][Show][Season][v-1]
         document.getElementsByClassName("Episodes").item(0).appendChild(x);
     }
 
     document.getElementsByClassName("VideoPlayer").item(0).src = Episode;
     document.getElementsByClassName("Episodes").item(0).value = Episode;
+    document.getElementsByClassName("Seasons").item(0).value = Season;
 }
 
 function Page_Unload() {
     window.localStorage.setItem("CurrentEpisode", Episode)
     window.localStorage.setItem("CurrentShow", Show)
+    window.localStorage.setItem("CurrentSeason", Season)
 }
 
 function EpisodeSelect() {
@@ -60,6 +84,13 @@ function EpisodeSelect() {
 
 function ShowSelect() {
     Show = document.getElementsByClassName("Shows").item(0).value;
-    Episode = wave_ShowList[Show][0];
+    Season = "1";
+    Episode = DataBase["Shows"][Show][Season][0];
+    document.location.reload(true);
+}
+
+function SeasonSelect() {
+    Season = document.getElementsByClassName("Seasons").item(0).value;
+    Episode = DataBase["Shows"][Show][Season][0];
     document.location.reload(true);
 }
